4a645207b64e3d484739998793414879e1c5975c
Poorna-Vishwanath <53818189+Poorna-Vishwanath@users.noreply.github.com>
2019-08-07 13:15:31 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,20 +1,43 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 int main()
-{
-	int a[20],i,n;
-	cout<<"Enter the number of elements:";
-	cin>>n;
-	cout<<"Enter the numbers:";
-	for(i=0;i<n;i++)
-		cin>>a[i];
-	int max=a[0];
-	for(i=0;i<n;i++)
+{ 
+	int i,arr1[10],arr2[10];
+	int n1,n2,k1,k2;
+	cout<<"Enter the number of elements for 1st array:";
+	cin>>n1;
+	cout<<"Enter the elements:";
+	for(i=0;i<n1;i++)
+		cin>>arr1[i];
+	cout<<"Enter the key element to be searched:";
+	cin>>k1;
+	for(i=0;i<n1;i++)
 	{
-		if(a[i]>max)
-			max=a[i];
+		if(k1==arr1[i])
+			{  cout<<"1";
+			   break;
+			}
+		if(i==arr1[n1]-1)
+			cout<<"-1"<<endl;
 	}
-	cout<<"Maximum number:"<<max<<endl;
-	//cout<<a[n-1]<<endl;
+			
+	cout<<"Enter the number of elements for 2nd array:";
+	cin>>n2;
+	cout<<"Enter the elements:";
+	for(i=0;i<n2;i++)
+		cin>>arr2[i];
+	cout<<"Enter the key element to be searched:";
+	cin>>k2;
+	for(i=0;i<n2;i++)
+	{
+		if(k2==arr2[i])
+	       {  cout<<"1";
+		      break;
+		   }
+		if(i==arr2[n2]-1)
+			cout<<"-1";
+	}
+			
+	return 0;
 }
