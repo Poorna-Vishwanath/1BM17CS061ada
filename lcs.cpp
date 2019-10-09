7888b47f2fab2aa279322eceb7b580feaa096f1f
Poorna-Vishwanath <53818189+Poorna-Vishwanath@users.noreply.github.com>
2019-10-09 12:35:12 +0530

    Update lcs.cpp

--- lcs.cpp
+++ lcs.cpp
@@ -1,34 +1,56 @@
-#include <bits/stdc++.h> 
+#include<iostream> 
+#include<cstring> 
+#include<cstdlib> 
 using namespace std; 
   
-int max(int a, int b);  
-int lcs( char *X, char *Y, int m, int n )  
-{  
-    if (m == 0 || n == 0)  
-        return 0;  
-    if (X[m-1] == Y[n-1])  
-        return 1 + lcs(X, Y, m-1, n-1);  
-    else
-        return max(lcs(X, Y, m, n-1), lcs(X, Y, m-1, n));  
-}  
+
+void lcs( char *X, char *Y, int m, int n ) 
+{ 
+   int L[m+1][n+1]; 
+   for (int i=0; i<=m; i++) 
+   { 
+     for (int j=0; j<=n; j++) 
+     { 
+       if (i == 0 || j == 0) 
+         L[i][j] = 0; 
+       else if (X[i-1] == Y[j-1]) 
+         L[i][j] = L[i-1][j-1] + 1; 
+       else
+         L[i][j] = max(L[i-1][j], L[i][j-1]); 
+     } 
+   } 
+  
+   int index = L[m][n]; 
+   char lcs[index+1]; 
+   lcs[index] = '\0'; 
+  
+   int i = m, j = n; 
+   while (i > 0 && j > 0) 
+   { 
+      
+      if (X[i-1] == Y[j-1]) 
+      { 
+          lcs[index-1] = X[i-1];
+          i--; j--; index--;     
+      } 
+  
+      
+      else if (L[i-1][j] > L[i][j-1]) 
+         i--; 
+      else
+         j--; 
+   } 
+   
+   cout << "LCS of " << X << " and " << Y << " is " << lcs<<endl; 
+} 
   
 
-int max(int a, int b)  
-{  
-    return (a > b)? a : b;  
-}  
-  
-
-int main()  
-{  
-    char X[]="bqdrcvefgh";
-    char Y[]="abcvdefgh";
-    
-      
-    int m = strlen(X);  
-    int n = strlen(Y);  
-      
-    cout<<"Length of LCS is "<< lcs( X, Y, m, n ) <<endl;  
-      
-    return 0;  
-}  
+int main() 
+{ 
+  char X[] = "BDCABA"; 
+  char Y[] = "ABCBDAB"; 
+  int m = strlen(X); 
+  int n = strlen(Y); 
+  lcs(X, Y, m, n); 
+  return 0; 
+} 
